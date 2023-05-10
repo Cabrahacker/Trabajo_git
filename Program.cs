ce760343441b919f10dfc3c1af62f6f8c76cb595
Cabrahacker <cgs0033631@est.univalle.edu>
2023-05-10 14:13:02 -0400

    Primera version de git

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Trabajo_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.Write("Ingresa un número: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingresa otro número: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma para git es: "+(n1+n2));
+
+        }
+    }
+}
