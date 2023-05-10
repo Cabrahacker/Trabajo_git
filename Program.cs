fb0abcb7560d856c9e77e58528a059483a30643b
Cabrahacker <cgs0033631@est.univalle.edu>
2023-05-10 14:22:02 -0400

    Segunda versión de git

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@
     {
         static void Main(string[] args)
         {
+            string name = "Sebastiàn :D", apellido = "Cabrera Gómez o_o", nombrecompleto;
             int n1, n2;
             Console.Write("Ingresa un número: ");
             n1 = int.Parse(Console.ReadLine());
             Console.Write("Ingresa otro número: ");
             n2 = int.Parse(Console.ReadLine());
             Console.WriteLine("La suma para git es: "+(n1+n2));
+            nombrecompleto = name + apellido;
+            Console.WriteLine(nombrecompleto);
 
         }
     }
